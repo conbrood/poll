cac38211725cd391dcc129748bcd8fd93eda044a
anaximander17 <71157308+anaximander17@users.noreply.github.com>
2020-11-01 14:16:52 -0600

    Update until.sh

--- until.sh
+++ until.sh
@@ -1,3 +1,9 @@
+//the purpose of this block of code is to wait until the conditions are right for it to be executed. The purpose of a until loop is to wait for the conditions/parameters 
+of the code to be right, and once theyre right the until loop is executed.
+
+
+
+
 #!/bin/bash
 
 counter=4
