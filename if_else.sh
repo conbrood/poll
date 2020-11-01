9ad21df20de759b497c3cfc3a4b2c01632dfacab
anaximander17 <71157308+anaximander17@users.noreply.github.com>
2020-11-01 13:32:51 -0600

    Update if_else.sh

--- if_else.sh
+++ if_else.sh
@@ -1,3 +1,7 @@
+//the purpose of this block of code is to choose between num_a and num_b depending on what the question is asking. Since the question the code is asking in line 10, 
+is if num_a is greater than num_b. If it is then the code will spit something about, it should be spitting an echo statement out but an echo statement is missing from the code.
+
+
 #!/bin/bash
 
 num_a=800
